129af70ed99b836a5e6bfc04a60b5547237d3d4c
Immigrant Defender <immigrant.defender@yandex.com>
2017-12-12 18:31:53 -0500

    wip: user auth stuff

--- migrations/001_users_and_authentication.sql
+++ migrations/001_users_and_authentication.sql
@@ -3,19 +3,13 @@
 --
 
 -- adapted from example in the postgrest docs
+-- link: https://postgrest.com/en/v4.3/auth.html#schema-isolation
 
 -- We put things inside the basic_auth schema to hide
 -- them from public view. Certain public procs/views will
 -- refer to helpers and tables inside.
 
--- TODO: configure DB to use app.jwt_secret
-
 CREATE schema IF NOT EXISTS basic_auth;
-
--- TODO: how to do IF NOT EXISTS so this doesn't raise an error
-CREATE type basic_auth.jwt_token AS (
-  token text
-);
 
 CREATE table IF NOT EXISTS
 basic_auth.users (
@@ -29,9 +23,9 @@ basic_auth.users (
 CREATE OR REPLACE function
 basic_auth.check_role_exists() returns trigger
   language plpgsql
-  as $$
+  AS $$
 BEGIN
-  IF NOT EXISTS (select 1 from pg_roles as r where r.rolname = new.role) THEN
+  IF NOT EXISTS (select 1 from pg_roles AS r where r.rolname = new.role) THEN
     RAISE foreign_key_violation USING message =
       'unknown database role: ' || new.role;
     RETURN NULL;
@@ -51,7 +45,7 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE OR REPLACE function
 basic_auth.encrypt_pass() returns trigger
   language plpgsql
-  as $$
+  AS $$
 BEGIN
   IF tg_op = 'INSERT' OR new.pass <> old.pass THEN
     new.pass = crypt(new.pass, gen_salt('bf'));
@@ -66,10 +60,12 @@ CREATE TRIGGER encrypt_pass
   FOR EACH ROW
   EXECUTE procedure basic_auth.encrypt_pass();
 
-CREATE OR REPLACE FUNCTION
-basic_auth.user_role(email text, pass text) RETURNS name
-  language plpgsql
-  as $$
+CREATE OR REPLACE FUNCTION basic_auth.user_role(
+  email text,
+  pass text
+) RETURNS name
+LANGUAGE plpgsql
+AS $$
 BEGIN
   RETURN (
   SELECT ROLE FROM basic_auth.users
@@ -79,28 +75,29 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE FUNCTION
-login(email text, pass text) RETURNS basic_auth.jwt_token
-  language plpgsql
-  as $$
+CREATE OR REPLACE FUNCTION basic_auth.login(
+  email text,
+  pass text
+) RETURNS jwt_token
+  LANGUAGE plpgsql
+  AS $$
 DECLARE
   _role name;
-  result basic_auth.jwt_token;
+  result jwt_token;
 BEGIN
+
   -- check email and password
-  SELECT basic_auth.user_role(email, pass) INTO _role;
+  SELECT basic_auth.user_role(CAST(email AS text), CAST(pass AS text)) INTO _role;
   IF _role IS NULL THEN
     RAISE invalid_password USING MESSAGE = 'invalid user or password';
   END IF;
 
-  SELECT sign(
-      row_to_json(r), app.jwt_secret
-    ) as token
-    FROM (
-      SELECT _role AS role, login.email AS email,
-         extract(epoch from now())::integer + 60 * 60 as exp
-    ) r
-    INTO result;
+  -- grab fields for JWT
+  -- encrypt and return JWT
+  SELECT email as token INTO result;
+
+  -- return the result
   RETURN result;
+
 END;
 $$;
